c94f549db1a7281a9c05e43ce3515f166c606cc0
Uliana Komarynska <uliakom@gmail.com>
2022-07-26 22:46:29 +0300

    Added files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
+
+export default App;
